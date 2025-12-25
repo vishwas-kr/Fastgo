8509a51b688e9d8a4260d602267c98fe7c4fc43f
kr-vishwas <vishwaskumar445@gmail.com>
2025-12-25 23:08:15 +0530

    Initial Commit

---
+++ Fastgo/FastgoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FastgoApp.swift
+//  Fastgo
+//
+//  Created by vishwas on 12/25/25.
+//
+
+import SwiftUI
+
+@main
+struct FastgoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
