f4b2acb9608116cae6f2704408a6b04e6e621342
kr-vishwas <vishwaskumar445@gmail.com>
2026-01-04 18:25:29 +0530

    App Constants

---
+++ Fastgo/Constants/AppConstants.swift
@@ -0,0 +1,32 @@
+//
+//  AppConstants.swift
+//  Fastgo
+//
+//  Created by vishwas on 12/25/25.
+//
+
+import Foundation
+
+struct AppImageConstants {
+    static let image1 = "https://plus.unsplash.com/premium_vector-1747611674632-34d5ff502ee2?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTh8fGJpa2V8ZW58MHwxfDB8fHww"
+    static let image2 = "https://plus.unsplash.com/premium_vector-1736409565156-0ba411fd1a0a?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjJ8fGJpa2V8ZW58MHwxfDB8fHww"
+    
+    static let image3 = "https://plus.unsplash.com/premium_vector-1724366105461-06925f20d872?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NjN8fGJpa2V8ZW58MHwxfDB8fHww"
+    
+    static let image4 = "https://plus.unsplash.com/premium_vector-1747668808416-ca3680f0f148?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NjJ8fGJpa2V8ZW58MHwxfDB8fHww"
+    
+    static let image5 = "https://plus.unsplash.com/premium_vector-1765373833220-e9019ed64e71?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDl8fGJpa2V8ZW58MHwxfDB8fHww"
+    
+    static let image6 = "https://plus.unsplash.com/premium_vector-1737556376200-dc0eae520e5c?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzJ8fGJpa2V8ZW58MHwxfDB8fHww"
+    
+
+    
+    
+    static let scooter1 = "https://plus.unsplash.com/premium_vector-1682306531167-ac8db94a7085?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8c2Nvb3RlcnxlbnwwfDF8MHx8fDA%3D"
+    
+    static let scooter2 = "https://plus.unsplash.com/premium_vector-1723111533101-93b43e0b0335?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHNjb290ZXJ8ZW58MHwxfDB8fHww"
+    
+    static let scooter3 = "https://plus.unsplash.com/premium_vector-1748084731480-1f48480a5ca5?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fHNjb290ZXJ8ZW58MHwxfDB8fHww"
+    
+    static let scooter4 = "https://plus.unsplash.com/premium_vector-1747119032880-9fb231af246f?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fHNjb290ZXJ8ZW58MHwxfDB8fHww"
+}
