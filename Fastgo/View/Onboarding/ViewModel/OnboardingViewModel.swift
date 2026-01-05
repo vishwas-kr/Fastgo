1a44e4054599a0ffdb81e1c9034eae182750b936
kr-vishwas <vishwaskumar445@gmail.com>
2026-01-05 21:15:36 +0530

    Added Onboarding Page Modal

--- Fastgo/View/Onboarding/ViewModel/OnboardingViewModel.swift
+++ Fastgo/View/Onboarding/ViewModel/OnboardingViewModel.swift
@@ -6,31 +6,6 @@
 //
 import Foundation
 
-enum OnboardingPage : Int, CaseIterable {
-    case firstPage
-    case secondPage
-    case thirdPage
-    
-    var title : String{
-        switch self {
-        case .firstPage : return "Beat the traffic"
-        case .secondPage : return "Quick rides big vibes."
-        case .thirdPage : return "Just pure movement"
-        }
-    }
-    
-    var description : String {
-        switch self {
-        case .firstPage:
-            return "One tap is all it takes to turn any street into a shortcut."
-        case .secondPage:
-            return "Your city never slows down, and neither should you."
-        case .thirdPage:
-            return "Less fuel, less noise, more freedom in every mile."
-        }
-    }
-}
-
 class OnboardingViewModel : ObservableObject {
     
     @Published var currentPage: Int = 0
