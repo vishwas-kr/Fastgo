6b43670c55b746b2b98a309e5b3ab4d050955c3e
kr-vishwas <vishwaskumar445@gmail.com>
2026-01-13 23:32:42 +0530

    MyAccount View UI

---
+++ Fastgo/View/Profile/Components/MyAccountGenderBox.swift
@@ -0,0 +1,18 @@
+//
+//  MyAccountGenderBox.swift
+//  Fastgo
+//
+//  Created by vishwas on 1/13/26.
+//
+
+import SwiftUI
+
+struct MyAccountGenderBox: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyAccountGenderBox()
+}
